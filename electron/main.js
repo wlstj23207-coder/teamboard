fb2df810db7caa2502de1980d36f6bfbcb0212b4
Jin Kim <jinkim@Jinui-Macmini.local>
2026-03-07 04:36:24 +0900

    fix: 보드 자동 이동 수정

---
+++ electron/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    titleBarStyle: 'hiddenInset', // 맥 스타일
+    icon: path.join(__dirname, 'icon.png')
+  })
+
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:5173')
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'))
+  }
+}
+
+app.whenReady().then(createWindow)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
